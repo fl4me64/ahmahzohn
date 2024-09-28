836fa81339748c54dd78a19eeb738c0c276bc515
Kenny Chan <kennychan386@gmail.com>
2024-09-28 14:08:09 -0700

    Initial commit

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Product 1',
+        image: '/images/airpods.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 1,
+    },
+    {
+        _id: '2',
+        name: 'Product 2',
+        image: '/images/alexa.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 1,
+    },
+    {
+        _id: '3',
+        name: 'Product 3',
+        image: '/images/camera.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 1,
+    },
+    {
+        _id: '4',
+        name: 'Product 4',
+        image: '/images/mouse.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 1,
+    },
+    {
+        _id: '5',
+        name: 'Product 5',
+        image: '/images/phone.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 1,
+    },
+    {
+        _id: '6',
+        name: 'Product 6',
+        image: '/images/playstation.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 1,
+    }
+]
+
+export default products
