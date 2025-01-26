4201b3938c95ba2f3d5193c9899e316a74e0b4f9
fl4me64 <kennychan386@gmail.com>
2025-01-26 08:44:06 -0800

    Initial commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+                <Container>
+                    <Navbar.Brand as={Link} to='/'>Ahmahzohn</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ms-auto'>
+                            <Nav.Link as={Link} to='/cart'>
+                                <FaShoppingCart /> Cart
+                            </Nav.Link>
+                            <Nav.Link as={Link} to='/login'>
+                                <FaUser /> Sign In
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+  }
+  export default Header
---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Apple AirPods Bluetooth Wireless Earbuds',
+        image: '/images/airpods.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '2',
+        name: 'Amazon Alexa',
+        image: '/images/alexa.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 0,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '3',
+        name: 'Camera',
+        image: '/images/camera.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '4',
+        name: 'Computer Mouse',
+        image: '/images/mouse.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '5',
+        name: 'iPhone',
+        image: '/images/phone.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '6',
+        name: 'PlayStation 5',
+        image: '/images/playstation.jpg',
+        description:
+        'Description',
+        brand: 'Brand',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 10,
+    }
+]
+
+export default products
